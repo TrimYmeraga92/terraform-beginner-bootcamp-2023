2c5fbf33641ff1a386983d41b9c9983e603e1cba
TrimYmeraga92 <trimymeraga92@gmail.com>
2023-10-31 21:27:29 +0000

    #38 assets upload using for each

--- main.tf
+++ main.tf
@@ -22,12 +22,13 @@ terraform {
 
 
 module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.user_uuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.error_html_filepath
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
-  content_version     = var.content_version
+  content_version = var.content_version
+  assets_path = var.assets_path
 }
 
 
